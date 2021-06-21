ffc98ed1043725c10cb50dfd42077128268b9b7f
PersatLab <75851358+PersatLab@users.noreply.github.com>
2021-06-21 14:02:10 +0200

    Delete Motor_Protein_Localization directory

--- Motor_Protein_Localization/maxN.m
+++
@@ -1,7 +0,0 @@
-function [ MaxK ] = maxN( X, k )
-%maxk(X,k)return the K maximum values of a 1D array
-A=sort(X);
-d=length(A);
-MaxK=A(d-k+1:d);
-end
-
--- Motor_Protein_Localization/minN.m
+++
@@ -1,6 +0,0 @@
-function [ MinK ] = mink( X, k )
-%mink(X,k)returne the K minimum values of an 1D array
-A=sort(X);
-MinK=A(1:k);
-end
-
